df2ebacb6d07bb5045e79e20cda2391fdd78131d
SparksCSS <54341755+SparksCSS@users.noreply.github.com>
2020-02-18 08:28:43 -0500

    Update xccdf-xml2csv.py

--- xccdf-xml2csv.py
+++ xccdf-xml2csv.py
@@ -1,69 +1,74 @@
-#!/usr/bin/env python
+"""
+Importable function that accepts XCCDFs of STIG checks and returns a data structure
+which can be manipulated as required.
 
-###
-# (C) 2010 Adam Crosby
-# Licensed under:
-#  http://creativecommons.org/licenses/by-nc-sa/3.0/
-##
-import csv
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+Seeing issues with .Net STIG; recommend try/except to handle error.
+
+Adapted from:
+(C) 2010 Adam Crosby
+Licensed under:
+http://creativecommons.org/licenses/by-nc-sa/3.0/
+
+Adapted by: Arron Sparks and Tyson Bradley
+"""
 import xml.etree.ElementTree as ET
-xmlns = "http://checklists.nist.gov/xccdf/1.1"
-
-if len(sys.argv) != 2:
-	print "  XCCDF-xml2tsv converts XCCDF XML documents (such as DISA STIGs)"
-	print "    into easier to use Tab-Separated documents."
-	print "  Please run as '%s' <filename> and redirect output as needed." % sys.argv[0]
-	print "  Files should open easily in Excel."
-	print "  E.g.:\n\t %s U_Perimeter_Router_v8R2_manual.xccdf.xml > output.tsv" % sys.argv[0]
-	sys.exit(0)
-try:
-	xml = ET.parse(sys.argv[1])
-except Exception,e:
-	print "Error, unable to parse XML document.  Are you sure that's XCCDF?"
-	sys.exit(-1)
-
-benchmark = xml.getroot()
-check_list = []
-profile_name = "MAC-1_Classified"
-profiles = benchmark.findall("{%s}Profile" % xmlns)
-for profile in profiles:
-	if profile.get("id") == profile_name:
-		#<select idref="V-761" selected="true"/>
-		selects = profile.findall("{%s}select" % xmlns)
-		for select_tag in selects:
-			if select_tag.get("selected") == "true":
-				check_list.append(select_tag.get('idref'))
-
-groups = benchmark.findall("{%s}Group" % xmlns)
 
 
+def import_xccdf(_xccdf):
+    xmlns = "http://checklists.nist.gov/xccdf/1.2"
 
-csvfile = open('tmp.csv', 'wb')
-output = csv.writer(csvfile, dialect='excel')
-output.writerow(('STIG ID', 'Version', 'Rule Title', 'Title', 'Severity', 'Check Text', 'Fix Text', 'CCI'))
-for group in groups:
-	group_id = group.get("id")
-	if group_id in check_list:
-		title = group.find("{%s}title" % xmlns).text
-		severity = group.find("{%s}Rule" % xmlns).get("severity")
-		version = group.find("{%s}Rule/{%s}version" % (xmlns, xmlns)).text
-		rule_title = group.find("{%s}Rule/{%s}title" % (xmlns, xmlns)).text
-		desctag = "{%s}Rule/{%s}description" % (xmlns, xmlns)
-		fixtext = group.find("{%s}Rule/{%s}fixtext" % (xmlns, xmlns)).text
-		try:
-			check = group.find("{%s}Rule/{%s}check/{%s}check-content" % (xmlns, xmlns, xmlns)).text
-			cci = group.find("{%s}Rule/{%s}ident" % (xmlns, xmlns)).text
-		except:
-			check = "(Missing - did you use an OVAL benchmark instead of a Manual XCCDF?)"
-			cci = "(Missing CCI Number may be an older STIG)"
-		descriptiontext = group.find(desctag).text
-		encodedDesc = descriptiontext.replace("&gt;", ">").replace("&lt;", "<").replace("&", "&amp;")
-		innerXML = "<desc>%s</desc>" % format(encodedDesc)
-		xml = ET.XML(innerXML)
-		iacontrols = xml.find("IAControls").text
-		vulndisc = xml.find("VulnDiscussion").text
+    xml = ET.parse(_xccdf)
+    benchmark = xml.getroot()
+    check_list = []
+    profiles = benchmark.findall("{%s}Profile" % xmlns)
+    for profile in profiles:
+        selects = profile.findall("{%s}select" % xmlns)
+        for select_tag in selects:
+            if select_tag.get("selected") == "true":
+                check_list.append(select_tag.get('idref'))
 
-		output.writerow( (group_id.replace('\n', '##').replace('V-',''), version.replace('\n', '##'), rule_title.replace('\n', '##'), title.replace('\n', '##'), severity.replace('\n', '##'), check, fixtext, cci) )
+    groups = benchmark.findall("{%s}Group" % xmlns)
+    results = benchmark.findall("{%s}TestResult" % xmlns)
+    if type(results) == "<class 'list'>":
+        results = results[0]
+
+    stigs = {}
+    for group in groups:
+        group_id = group.get("id")
+        if group_id in check_list:
+            title = group.find("{%s}title" % xmlns).text
+            severity = group.find("{%s}Rule" % xmlns).get("severity")
+            version = group.find("{%s}Rule/{%s}version" % (xmlns, xmlns)).text
+            rule_title = group.find("{%s}Rule/{%s}title" % (xmlns, xmlns)).text
+            desctag = "{%s}Rule/{%s}description" % (xmlns, xmlns)
+            fixtext = group.find("{%s}Rule/{%s}fixtext" % (xmlns, xmlns)).text
+            descriptiontext = group.find(desctag).text
+            encodedDesc = descriptiontext.replace("&gt;", ">").replace("&lt;", "<").replace("&", "&amp;")
+            innerXML = "<desc>%s</desc>" % format(encodedDesc)
+            xml = ET.XML(innerXML)
+
+            nl = '\n'
+            stigs[f"{group_id.replace(nl, '##').replace('xccdf_mil.disa.stig_group_', '')}"] = {
+                'Version': f"{version.replace(nl, '##')}",
+                'Rule Title': f"{rule_title.replace(nl, '##')}",
+                'Title': f"{title.replace(nl, '##')}",
+                'Severity': f"{severity.replace(nl, '##')}",
+                'Fix Text': fixtext,
+            }
+
+    for stig in stigs:
+        version = stigs[stig]['Version']
+        for result in results:
+            if 'idref' in result.attrib and 'version' in result.attrib and version in result.attrib['version']:
+                status = result.find(("{%s}result" % xmlns))
+                stigs[stig]['Status'] = status.text
+
+    return stigs
+
+
+def main():
+    stigs = import_xccdf(input('Provide path to XCCDF: '))
+
+
+if __name__ == '__main__':
+    main()
